664bb327ab2920e64d36d5ff3c00074bafedd09c
Eliel Barrios <33172705+elielbarrios@users.noreply.github.com>
2021-09-24 16:00:18 -0600

    Add files via upload

---
+++ PrivateRoute.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import { Redirect, Route } from 'react-router-dom'
+
+
+export const PrivateRoute = ({
+    isAuthenticated,
+    component: Component,
+    ...rest
+}) => {
+
+    // console.log(rest.location.pathname);
+    // localStorage.setItem('lastpath', rest.location.pathname);
+
+    return (
+        <Route { ...rest } 
+            component = { (props) =>(
+                (isAuthenticated)
+                    ? <Component {...props} />
+                    : <Redirect to="/auth/login" />
+            )}
+        
+        />
+    )
+}
+
+
+PrivateRoute.propTypes = {
+    isAuthenticated: PropTypes.bool.isRequired,
+    component: PropTypes.func.isRequired
+}
